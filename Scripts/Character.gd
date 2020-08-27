d78595b064cf7d68180acf2ab90bc73d16accefc
Andrés Botero <0xafbf@gmail.com>
2020-08-26 22:56:27 -0500

    added multiplayer, changed to pick coins

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -2,8 +2,13 @@ extends KinematicBody2D
 
 class_name Character
 
-var tengo_llave = false
+# var tengo_llave = false
+var cantidad_monedas = 0
 var posicion_inicial = Vector2()
+
+export var player = 1
+
+export var speed = 500.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,14 +20,11 @@ func reiniciar():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var direction = Vector2()
+	if player == 1:
+		direction.x = Input.get_action_strength("MoveRight") - Input.get_action_strength("MoveLeft")
+		direction.y = Input.get_action_strength("MoveDown") - Input.get_action_strength("MoveUp")
+	else:
+		direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+		direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
-	if Input.get_action_strength("MoveLeft"):
-		direction.x = -500
-	if Input.get_action_strength("MoveRight"):
-		direction.x = 500
-	if Input.get_action_strength("MoveUp"):
-		direction.y = -500
-	if Input.get_action_strength("MoveDown"):
-		direction.y = 500
-	
-	move_and_slide(direction)
+	move_and_slide(direction * speed)
--- Scripts/Llave.gd
+++ Scripts/Llave.gd
@@ -1,8 +1,12 @@
 extends Area2D
 
 
+var moneda_tomada = false
 
 func _on_AreaLLave_body_entered(body):
 	if body is Character:
-		visible = false
-		body.tengo_llave = true
+		if not moneda_tomada:
+			visible = false
+			#body.tengo_llave = true
+			body.cantidad_monedas += 1
+			moneda_tomada = true
